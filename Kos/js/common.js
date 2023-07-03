b778c6baf4c2aeeefb1da0ddd92a31577c42f07b
Yu-Hui Lin <erinlin6699889@gmail.com>
2023-07-04 01:53:07 +0800

    分割CSS檔案

--- Kos/js/common.js
+++ Kos/js/common.js
@@ -1,17 +1,17 @@
 $(function() {
 
     // 當卷軸滑動位置超過60
-	/*if($(document).scrollTop() >= 60){
+	if($(document).scrollTop() >= 60){
 		$('#topbar').addClass('topbar-colorful');
-	}*/
+	}
 
     // 螢幕滑動事件
-    /*$(window).scroll(function() {
+    $(window).scroll(function() {
         if ($(this).scrollTop() >= 60) {
             $('#topbar').addClass('topbar-colorful');
         } else {
             $('#topbar').removeClass('topbar-colorful');
         }
-    });*/
+    });
 
 });
