a52f365381d18b79d8aaf9ddc488b951da665eab
Yu-Hui Lin <erinlin6699889@gmail.com>
2023-07-04 22:54:48 +0800

    商店框架

--- Kos/js/common.js
+++ Kos/js/common.js
@@ -1,17 +1,10 @@
-$(function() {
 
-    // 當卷軸滑動位置超過60
-	if($(document).scrollTop() >= 60){
-		$('#topbar').addClass('topbar-colorful');
-	}
+// 開啟手機版選單
+function ShowBurgerMenu() {
+    $('#menu').show();
+}
 
-    // 螢幕滑動事件
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 60) {
-            $('#topbar').addClass('topbar-colorful');
-        } else {
-            $('#topbar').removeClass('topbar-colorful');
-        }
-    });
-
-});
+// 關閉手機版選單
+function HideBurgerMenu() {
+    $('#menu').hide();
+}
---
+++ Kos/js/index.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    // 當卷軸滑動位置超過60
+	if($(document).scrollTop() >= 60){
+		$('#topbar').addClass('topbar-colorful');
+	}
+
+    // 螢幕滑動事件
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 60) {
+            $('#topbar').addClass('topbar-colorful');
+        } else {
+            $('#topbar').removeClass('topbar-colorful');
+        }
+    });
+
+});
