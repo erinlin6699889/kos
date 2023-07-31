cfc5d9d1b31ac5dd125d2c673dedeb2d42329ccf
Yu-Hui Lin <erinlin6699889@gmail.com>
2023-08-01 01:20:30 +0800

    購物車JS完成

--- Kos/js/common.js
+++ Kos/js/common.js
@@ -42,7 +42,9 @@ function IncreaseCartNumber(id) {
             list.count += 1;
         }
     });
-    console.log(CartItemsList)
+
+    // 更新購物車資訊
+    UpdateCartData();
 }
 
 // 減少購物車商品數量
@@ -60,11 +62,55 @@ function ReduceCartNumber(id) {
                 list.count -= 1;
             }
         });
-        console.log(CartItemsList)
+
+        // 更新購物車資訊
+        UpdateCartData();
     }
 }
 
 // 刪除購物車內商品
 function RemoveCartItem(id) {
+    if(confirm('確定要將此商品從購物車移除嗎？')){
+        // 更新Common.js定義的物件
+        CartItemsList.forEach((list, i, arr) => {
+            // 用id比對商品，增加數量
+            if(list.id == id) {
+                CartItemsList.splice(i, 1);  // 刪除指定索引值的陣列
+                // 刪除購物車內容
+                $(`#cart-item-list .cart-content-table .cart-table > tbody > tr:nth-child(${i + 1})`).remove();
+            }
+        });
 
+        // 如果購物車清零，顯示無商品
+        if(CartItemsList.length == 0){
+            // 清空購物車
+            $('#cart-no-item').show();
+            $('#cart-item-list').hide();
+            $('#checkout-btn').hide();
+        }
+    }
+
+    // 更新購物車資訊
+    UpdateCartData();
+}
+
+// 更新購物車資訊
+function UpdateCartData(){
+    // 更新商品數量總和和總價
+    let TotalCount = 0;
+    let TotalPrice = 0;
+    if(CartItemsList.length > 0){
+        CartItemsList.forEach((item, i, arr)=>{
+            TotalCount += item.count;
+            TotalPrice += item.count * item.price;
+        });
+    }
+
+    $('#total-count').text(TotalCount);
+    $('#total-price').text(TotalPrice);
+
+    // 更新購物車總數字
+    CartItemsCount = TotalCount;
+    $('#pc-cart').text(CartItemsCount);
+    $('#mobile-cart').text(CartItemsCount);
 }
--- Kos/js/product.js
+++ Kos/js/product.js
@@ -67,15 +67,17 @@ function AddItemToCart(id) {
     // 載入商品資訊
     $('#current-items').text($('#product-name').text());
     $('#current-count').text($('#number').text());
-    $('#current-price').text('$' + parseInt($('#product-price').text().split(' ')[1]) * parseInt($('#number').text()));
+    $('#current-price').text('$' + parseInt($('#product-price').text()) * parseInt($('#number').text()));
 
     // 檢查同商品是否已在購物車內
     let CartExistItem = false;
+    let CartItemsIndex = 0;  // 商品陣列索引值
     if(CartItemsList.length > 0){
         CartItemsList.forEach((item, i, arr) => {
             if(item.id == id) {
                 CartExistItem = true;
                 item.count += parseInt($('#number').text());  // 加總數字
+                CartItemsIndex = i;
             }
         })
     }
@@ -87,6 +89,7 @@ function AddItemToCart(id) {
             count: parseInt($('#number').text()),
             price: parseInt($('#product-price').text())
         });
+        CartItemsIndex = CartItemsList.length - 1;
     } 
 
     // 更新介面上顯示的數字
@@ -101,10 +104,50 @@ function AddItemToCart(id) {
 
     // 更新購物車HTML
     $('#cart-item-list .cart-content-table .cart-table > tbody').html(`
-    
+        <tr>
+            <td>
+                <div class="cart-column1">
+                    <div class="cart-img-cover">
+                        <img src="asset/shop-01.png" />
+                    </div>
+                </div>
+                <div class="cart-column2">
+                    <div class="cart-name">${$('#product-name').text()}</div>
+                    <div class="cart-count">
+                        <div>
+                            <a href="javascript:void(0)" onclick="ReduceCartNumber(${id})">
+                                <img class="minus-icon" src="asset/minus.png" />
+                            </a>
+                        </div>
+                        <div class="number-label">
+                            <label id="number-${id}" class="cart-number">${CartItemsList[CartItemsIndex].count}</label>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0)" onclick="IncreaseCartNumber(${id})">
+                                <img class="plus-icon" src="asset/plus.png" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-column3">
+                    <div class="cart-price">$ <span id="cart-price-${id}">${CartItemsList[CartItemsIndex].count * CartItemsList[CartItemsIndex].price}</span></div>
+                    <div class="cart-remove">
+                        <a href="javascript:void(0)" onclick="RemoveCartItem(1)"><img src="asset/trash.png" /></a>
+                    </div>
+                </div>
+            </td>
+        </tr>
     `);
+    
 
+    // 更新購物車資訊
+    UpdateCartData();
+
+    // 顯示提示視窗
     $('#hint-panel').show();
+
+    // 重製商品數字
+    $('#number').text(1);
 }
 
 // 顯示商品
